88ac120c8594fa84e746e6fac42a000be6051e13
Shivam Chaubey <shivamchaubey027@gmail.com>
2025-06-05 21:25:29 +0530

    vercel

--- api/index.py
+++ api/index.py
@@ -4,3 +4,4 @@ os.environ.setdefault('SESSION_SECRET', 'prod-secret-key')  # update in prod!
 os.environ.setdefault('DATABASE_URL', 'sqlite:///quiz_system.db')
 
 from app import app as application
+app = app
